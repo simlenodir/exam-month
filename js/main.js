77d07fae08fb7fa6bc6c9f06a15fdc4610940f63
simlenodir <nodirsmailov0@gmail.com>
2022-06-20 18:31:37 +0300

    something added

--- js/main.js
+++ js/main.js
@@ -16,38 +16,42 @@ const elSearchForm = document.getElementById("search-parrots");
 
 // start cards buttons
 // start edit modals
-const elEditData = document.getElementById("add-parrot1");
-const modaleditTitle = document.getElementById("parrot-title1");
-const modaleditImg = document.getElementById("parrot-img1");
-const modaleditPrice = document.getElementById("price1");
-const modaleditBirthDate = document.getElementById("parrot-date1");
-const modaleditWidth = document.getElementById("parrot_width1");
-const modaleditHeight = document.getElementById("parrot_height1");
-const modaleditFeatures = document.getElementById("features1");
-let   parrotsCount = document.getElementById('parrots-count')
+let elEditData = document.getElementById("add-parrot1");
+let modaleditTitle = document.getElementById("parrot-title1");
+let modaleditImg = document.getElementById("parrot-img1");
+let modaleditPrice = document.getElementById("price1");
+let modaleditBirthDate = document.getElementById("parrot-date1");
+let modaleditWidth = document.getElementById("parrot_width1");
+let modaleditHeight = document.getElementById("parrot_height1");
+let modaleditFeatures = document.getElementById("features1");
+let parrotsCount = document.getElementById("parrots-count");
 
-parrotsCount.textContent = 'counts'+ ': ' +products.length
+parrotsCount.textContent = "counts" + ": " + products.length;
 
 const renderParrots = (products) => {
   elWrapperListParrots.innerHTML = null;
   products.forEach((product) => {
     let elCard = elParrotTemplet.cloneNode(true);
-    let elCardBox = elCard.querySelector('.card')
+    let elCardBox = elCard.querySelector(".card");
     let elImgCard = elCard.querySelector(".card-img-top");
     let nameParrots = elCard.querySelector(".card-title");
     let cost = elCard.querySelector(".card-text");
     let size = elCard.querySelector(".badge");
     let elYear = elCard.querySelector(".born");
+    // let features = elCard.querySelector(".badg") madsjiojoijkjakldjfalk;sj
+    let featuresWrapp = elCard.querySelector('.features-wrapp')
     let editBtn = elCard.querySelector(".edit");
-    
-    elCardBox.dataset.id = product.id
+    let likedBtn = elCard.querySelector('.liked')
+
+    elCardBox.dataset.id = product.id;
     elImgCard.src = product.img;
     nameParrots.textContent = product.title;
     cost.textContent = "$" + product.price;
     size.textContent = product.sizes.width + " x " + product.sizes.height;
     elYear.textContent = product.birthDate;
     editBtn.dataset.id = product.id;
-    // console.log(editBtn.dataset.id);
+    likedBtn.dataset.id =product.id;
+    console.log(likedBtn.dataset.id);
     let elItem = document.createElement("li");
     elItem.className = "col-6 item";
     elItem.dataset.id = product.id;
@@ -55,15 +59,19 @@ const renderParrots = (products) => {
     elFragmentWrapper.appendChild(elItem);
     elWrapperListParrots.appendChild(elFragmentWrapper);
 
-    editBtn.addEventListener('click', (e)=>{
-        e.preventDefault()
-        elEditData.dataset.id = editBtn.dataset.id
-      
-        console.log(elEditData.dataset.id );
-      })
-      
-   
+    elFeaturesTemp = elCard.querySelector('.features-temp')
+    console.log(elFeaturesTemp);
+
+    editBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      elEditData.dataset.id = editBtn.dataset.id;
+
+      console.log(elEditData.dataset.id);
+    });
   });
+
+ 
+
 };
 renderParrots(products);
 function handleAddCard(evt) {
@@ -126,68 +134,59 @@ function handleSearch(evt) {
   let foundParrots = products.filter((parrot) => parrot.title.match(regex));
   elWrapperListParrots.innerHTML = "";
   renderParrots(foundParrots);
-  
+
   products.sort(sortFunction[sort]);
 }
+let copyCard = [];
 
+function handleChange(evt) {
+  if (evt.target.matches(".delete")) {
+    let deletedItem = evt.target.closest("li");
+    let itemId = deletedItem.dataset.id;
+    let deletCard = products.filter((product) => product.id != itemId);
+    products = deletCard;
+    renderParrots(deletCard);
+  }
+  if (evt.target.matches(".edit")) {
+    copyCard = [];
+    let copiedCard = products.find((item) => evt.target.dataset.id == item.id);
+    copyCard.push(copiedCard);
 
-
-function handleChange (evt) {
-  if (evt.target.matches('.delete')) {
-    let deletedItem = evt.target.closest('li')
-    let itemId = deletedItem.dataset.id
-    let deletCard = products.filter((product)=> product.id !=itemId)
-    products = deletCard
-    renderParrots (deletCard)
+    modaleditTitle.value = copiedCard.title;
+    modaleditPrice.value = copiedCard.price;
+    modaleditBirthDate.value = copiedCard.birthDate;
+    modaleditWidth.value = copiedCard.sizes.width;
+    modaleditHeight.value = copiedCard.sizes.height;
+    modaleditFeatures.value = copiedCard.features;
+    console.log(copyCard);
+  }
+  if (evt.target.matches(".liked")) {
+    // likedCard = [];
+    let likedCard = products.find((item) => evt.target.dataset.id == item.id);
+    let changedCOl = document.querySelector('.changed')
+    console.log(changedCOl);
+    changedCOl.classList.add('fa-solid')
+    changedCOl.classList.add('fa-star')
   }
 }
-//
+
 function changeCardContent(evt) {
   evt.preventDefault();
-  // console.log(modaleditTitle.value);
-  let findEdite = products.find((e)=> e.id == elEditData.dataset.id )
-  // console.log(elEditData);
-  let edittitle = modaleditTitle.value.trim();
-  let editPrice = modaleditPrice.value.trim();
-  let editBrithDate = modaleditBirthDate.value.trim();
-  let editWidth = modaleditWidth.value.trim();
-  let editHeight = modaleditHeight.value.trim();
-  let editFeatures = modaleditFeatures.value.trim();
-  
-findEdite = [{
-title: edittitle,
-img: "https://media.istockphoto.com/photos/amazon-rainforest-parrot-macaw-picture-id1197182594?b=1&k=20&m=1197182594&s=170667a&w=0&h=bBQfSDgofCr_w2DBf79cwQe-JA45i02vCv7Ttx5qcmU=",
-price: editPrice ? editPrice :price,
-// birthDate: editBrithDate ? editBrithDate : birthDate,
-sizes: {
-  width: editWidth ? editWidth : width,
-  height: editHeight ? editHeight : height,
-},
-features: editFeatures ? editFeatures : features,
-}];
-products.push(findEdite)
-renderParrots(products);
-console.log(findEdite);
+  console.log("ok");
 
+  console.log(copyCard);
+
+  copyCard[0].title = modaleditTitle.value;
+  copyCard[0].price = modaleditPrice.value;
+  copyCard[0].birthDate = modaleditBirthDate.value;
+  copyCard[0].sizes.width = modaleditWidth.value;
+  copyCard[0].sizes.width = modaleditHeight.value;
+  copyCard[0].features = modaleditFeatures.value;
+
+  renderParrots(products);
 }
-elEditData.addEventListener("submit", changeCardContent);
 
+elEditData.addEventListener("submit", changeCardContent);
 elAddCardData.addEventListener("submit", handleAddCard);
 elSearchForm.addEventListener("submit", handleSearch);
 elWrapperListParrots.addEventListener("click", handleChange);
-
-// let edittitle  = modaleditTitle.value.trim();
-// let editPrice  = modaleditPrice.value.trim();
-// let editBrithDate  = modaleditBirthDate.value.trim();
-// let editWidth  = modaleditWidth.value.trim();
-// let editHeight  = modaleditHeight.value.trim();
-// let editFeatures  = modaleditFeatures.value.trim();
-// if (evt.target.dataset.id = product.id) {
-//     product.title = edittitle
-//     product.price ="$" + editPrice
-//     product.birthDate = editBrithDate
-//     product.sizes.width = editWidth
-//     product.sizes.height = editHeight
-//     product.features = editFeatures
-//     //   console.log(product.title);
-// }
